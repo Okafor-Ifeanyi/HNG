5fe7c56988b511c79fab1b5615c8663d38dadc43
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2023-09-09 01:50:02 +0100

    Initial Commit + Added functionalities and Connected to Cluster MongoDB

---
+++ src/DB/connect.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const connectDb = async () => {
+    const MONGO_URI = process.env.MONGO_URI ;
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
+      family: 4,
+    })
+    .then(() => {
+        console.log('Database Connected!!!')
+    })
+    .catch((err) => {
+      process.exit(1);
+    })
+  };
+
+// create a model from schema and export it
+export default connectDb
